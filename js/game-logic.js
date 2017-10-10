81c7cdaa4c8436d389fb02244c9a2a54c5213067
Robert Tuck <robtuck@gmail.com>
2017-10-10 16:18:37 -0400

    Passed Player Moves and setPlayerMoves() - Main Functionality

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,35 @@
 // All code should be written in this file.
+var playerOneMoveOneType;
+var playerOneMoveTwoType;
+var playerOneMoveThreeType;
+var playerTwoMoveOneType;
+var playerTwoMoveTwoType;
+var playerTwoMoveThreeType;
+var playerOneMoveOneValue;
+var playerOneMoveTwoValue;
+var playerOneMoveThreeValue;
+var playerTwoMoveOneValue;
+var playerTwoMoveTwoValue;
+var playerTwoMoveThreeValue;
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+  var moves = ['rock', 'paper', 'scissors'];
+  var moveTypes = [moveOneType, moveTwoType, moveThreeType];
+  var moveValues = [moveOneValue, moveTwoValue, moveThreeValue];
+
+  if (player === 'Player One') {
+    playerOneMoveOneType = moveOneType;
+    playerOneMoveTwoType = moveTwoType;
+    playerOneMoveThreeType = moveThreeType;
+    playerOneMoveOneValue = moveOneValue;
+    playerOneMoveTwoValue = moveTwoValue;
+    playerOneMoveThreeValue = moveThreeValue;
+  } else if (player === 'Player Two') {
+    playerTwoMoveOneType = moveOneType;
+    playerTwoMoveTwoType = moveTwoType;
+    playerTwoMoveThreeType = moveThreeType;
+    playerTwoMoveOneValue = moveOneValue;
+    playerTwoMoveTwoValue = moveTwoValue;
+    playerTwoMoveThreeValue = moveThreeValue;
+  }
+};
